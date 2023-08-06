615ea35e189ad2b71a5ecb9b0809122aae222172
SomaTomita <134721775+SomaTomita@users.noreply.github.com>
2023-08-06 18:45:18 +0900

    add Create New Account form

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -1,23 +1,24 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Quiz from "./quiz-components/Quiz/Quiz";
 import Section from "./quiz-components/Section/Section";
 import CreateQuiz from "./quiz-components/CreateQuiz/CreateQuiz";
 import CreateSection from "./quiz-components/CreateSection/CreateSection";
-import Login from "./login-componets/UserLogin/Login"
+import Login from "./login-componets/UserLogin/Login";
+import UserLogin from "./login-componets/UserLogin/UserLogin";
 
 function App() {
-    return (
-<Router>
-  <Routes>
-  <Route path="/login" element={<Login/>} /> 
-   <Route path="/create-quiz" element={<CreateQuiz/>} /> 
-   <Route path="/create-section" element={<CreateSection/>} /> 
-   <Route path="/sections" element={<Section />} /> 
-   <Route path="/sections/:sectionId/quizzes" element={<Quiz />} />
-  </Routes>
-</Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login-create" element={<Login />} />
+        <Route path="/login-user" element={<UserLogin/>} /> 
+        <Route path="/create-quiz" element={<CreateQuiz />} />
+        <Route path="/create-section" element={<CreateSection />} />
+        <Route path="/sections" element={<Section />} />
+        <Route path="/sections/:sectionId/quizzes" element={<Quiz />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
