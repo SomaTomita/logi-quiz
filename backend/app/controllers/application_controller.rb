c053d9a19293c783f2ebb1b8a803446f8357a346
SomaTomita <134721775+SomaTomita@users.noreply.github.com>
2023-08-07 23:37:45 +0900

    fix login

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+    # 全てのアクションが実行される前に、authenticate_requestを実行する
+  before_action :authenticate_request
+
+  private
+
+  def authenticate_request
+    # リクエストヘッダからAuthorizationトークンを取得し、最後の部分をトークンとして取得
+    token = request.headers['Authorization']&.split(' ')&.last
+    # もしトークンが存在しなければ、エラーメッセージを返す
+    return render json: { status: 'ERROR', message: 'Token is missing' }, status: :unauthorized if token.nil?
+
+    # JWTのデコードにはアプリケーションの秘密鍵とHS256アルゴリズムを使用
+    decoded_token = JWT.decode(token, Rails.application.secrets.secret_key_base, true, { algorithm: 'HS256' })
+    # デコードしたトークンからユーザーIDを取得し、そのユーザーを@current_userとして設定
+    @current_user = User.find(decoded_token[0]["user_id"])
+  # もしJWTのデコードに失敗したら、エラーメッセージを返す
+  rescue JWT::DecodeError
+    render json: { status: 'ERROR', message: 'Token is invalid' }, status: :unauthorized
+  end
 end
--- frontend/app/src/login-componets/UserLogin/SignIn.jsx
+++ frontend/app/src/login-componets/UserLogin/SignIn.jsx
@@ -1,28 +1,33 @@
 import "./Login.css";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 
-function Login() {
+function SignIn() {
   const {register, handleSubmit, formState: { errors } } = useForm({ mode: "onBlur" });
 
-  const onSubmit = data => {
-    axios.post('http://localhost:3001/users', data)
-      .then(response => {
-        console.log(response);
-      }).catch(error => {
-        console.error(error);
-      });
+  const navigate = useNavigate();
+  const goToNewAccount = () => {
+    navigate('/signupfor');
+  };
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post('http://localhost:3001/signin', data)
+      if(response.data.status === 'SUCCESS') {
+        navigate('/sections');
+      } else {
+        console.error('Login failed.');
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)} className="form-container">
-      <h1>Create New Account</h1>
-        <label htmlFor="name">
-          name:
-          <input id="name" type="text" {...register("name", { required: "名前は必須です。" })} />
-          <p>{ errors.name?.message }</p>
-        </label>
+      <h1>Sign in Form</h1>
         <label htmlFor="email">
           email:
           <input id="email" type="email" {...register("email", { required: "E-mailアドレスは必須です。" })}/>
@@ -35,9 +40,10 @@ function Login() {
         </label>
 
         <button type="submit">Submit</button>
+        <button onClick={goToNewAccount}>Create New Account</button>
       </form>
     </div>
   );
 }
 
-export default Login;
+export default SignIn;
