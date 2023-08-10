8fbe4de27c0bb75b2dbb8417453a9a24b6a04920
SomaTomita <134721775+SomaTomita@users.noreply.github.com>
2023-08-11 02:03:38 +0900

    fix login backend

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -11,6 +11,10 @@ module Api
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+    config.middleware.use ActionDispatch::Flash
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
