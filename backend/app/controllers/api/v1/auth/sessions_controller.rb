5f70ad2f627344b88ea492951e6c8e081a330ac3
SomaTomita <134721775+SomaTomita@users.noreply.github.com>
2023-08-10 14:36:51 +0900

    fix controller

--- backend/app/controllers/api/v1/auth/sessions_controller.rb
+++ backend/app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,6 +1,8 @@
 # ログイン状態確認用コントローラー
 class Api::V1::Auth::SessionsController < ApplicationController
+    # indexアクションでログイン状態を確認
     def index
+      # current_api_v1_userはログイン中のユーザー情報を取得するメソッド
       if current_api_v1_user
         render json: { is_login: true, data: current_api_v1_user }
       else
--- backend/app/controllers/sections_controller.rb
+++ backend/app/controllers/sections_controller.rb
@@ -1,7 +1,7 @@
 class SectionsController < ApplicationController
   def index
     sections = Section.all
-    render json: sections.to_json(only: [:id, :section_name])
+    render json: sections.to_json(only: [:id, :section_name]) # すべてのセクションを取得
   end
 
   def create
