16a8ddbfa6f6ea04b54de004f417750eb67df84d
SomaTomita <134721775+SomaTomita@users.noreply.github.com>
2023-08-11 18:17:06 +0900

    fix login form

--- backend/app/controllers/auth/registrations_controller.rb
+++ backend/app/controllers/auth/registrations_controller.rb
@@ -1,9 +1,18 @@
 # アカウント作成用コントローラー
 class Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  wrap_parameters false 
+
+  # コールバックを追加
+  after_action :log_errors, only: [:create], if: -> { response.status == 500 }
+
   private
 
   def sign_up_params
-    # サインアップ時に登録できるカラムを指定
-    params.require(:registration).permit(:email, :password, :password_confirmation)
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # エラーログ出力用のメソッドを追加
+  def log_errors
+    Rails.logger.info "User creation failed: #{resource.errors.full_messages.join(', ')}" if resource.errors.any?
   end
 end
