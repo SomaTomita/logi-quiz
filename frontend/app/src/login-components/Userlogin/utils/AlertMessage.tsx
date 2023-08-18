2d42c7961e3775ae447509ef1e1987a893762ff8
SomaTomita <ldh.eeexile@icloud.com>
2023-08-18 23:39:53 +0900

    add comment

--- frontend/app/src/login-components/Userlogin/utils/AlertMessage.tsx
+++ frontend/app/src/login-components/Userlogin/utils/AlertMessage.tsx
@@ -2,15 +2,18 @@ import Snackbar, { SnackbarCloseReason } from "@mui/material/Snackbar";
 import MuiAlert, { AlertProps } from "@mui/material/Alert";
 import { forwardRef } from "react";
 
+
+// カスタムのアラートメッセージコンポーネントを作成
 const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert( // 引数 = refが指すDOM要素の型, Alertコンポーネントが受け取るpropsの型
+ // forwardRefを使うことで、親コンポーネント（AlertMessage）が、子コンポーネント(MuiAlert)にアクセスすることが可能に
   props,
   ref,
 ) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-}); // shadowの深さ6, コンポーネントの参照を取得するためのref, 背景が塗りつぶし, Alertコンポーネントに渡される他のすべてのpropsを展開
+}); // shadowの深さ6, コンポーネントの参照を取得するためのref, 背景が塗りつぶし, Alertコンポーネントに渡されるonCloseやseverity等のすべてのpropsを展開
 
 interface AlertMessageProps { // Props の型定義
-  open: boolean; // Snackbar（アラートメッセージ）が表示されるかどうかのフラグ
+  open: boolean; // Snackbar（アラートメッセージ）が表示されるかどうかを制御
   setOpen: Function;  // 上記の open フラグを設定するための関数
   severity: "error" | "success" | "info" | "warning"; // アラートの種類（エラー、成功など）
   message: string; // アラートに表示するメッセージ内容
@@ -21,7 +24,7 @@ const AlertMessage = ({ open, setOpen, severity, message }: AlertMessageProps) =
   const handleCloseAlertMessage = (event: React.SyntheticEvent | Event, reason?: SnackbarCloseReason) => { //
     if (reason === "clickaway") return; // クリックすればすぐ消えるclickawayでSnackbarが閉じられた場合何もしない
 
-    setOpen(false); // それ以外はアラートを閉じる
+    setOpen(false); // それ以外はアラート(Snackbar)を閉じる
   };
 
   return (
