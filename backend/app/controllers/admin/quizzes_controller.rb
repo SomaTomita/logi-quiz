d3613536be32aff0ccf1e3a5e1334270435aa007
SomaTomita <ldh.eeexile@icloud.com>
2023-09-03 23:17:01 +0900

    test question model

--- backend/app/controllers/admin/quizzes_controller.rb
+++ backend/app/controllers/admin/quizzes_controller.rb
@@ -1,11 +1,13 @@
 class Admin::QuizzesController < ApplicationController
-    before_action :ensure_admin
+    before_action :ensure_admin 
     before_action :set_question, only: [:show, :update, :destroy]
 
+    # クイズの一覧表示アクション
     def index
         if params[:section_id]
+            # section_idに基づいてセクション、関連付けられているすべてのクイズを取得し、JSON形式で返す
             section = Section.find(params[:section_id])
-            quizzes = section.questions # セクションに関連付けられているすべてのクイズを取得する
+            quizzes = section.questions
             render json: quizzes, status: :ok
         else
             render json: { error: "Section ID is required" }, status: :bad_request
@@ -14,9 +16,10 @@ class Admin::QuizzesController < ApplicationController
 
     def create
         section = Section.find(params[:section_id])
+        # 新しいクイズを上で特定したセクションに関連付けて作成
         @question = section.questions.new(question_params)
         # Question モデルは has_one :explanation の関連付けであるので、build_explanationで、questionに紐づいた新しい Explanation オブジェクトをビルド
-        explanation = @question.build_explanation(explanation_text: params[:quiz][:explanation_text]) # pramsでquizにあるexplanation_textを取得
+        explanation = @question.build_explanation(explanation_text: params[:quiz][:explanation_text]) # paramsでquizにあるexplanation_textを取得
       
         if @question.save
           render json: @question, status: :created
@@ -27,9 +30,11 @@ class Admin::QuizzesController < ApplicationController
 
     def show
         @question = Question.includes(:choices, :explanation).find(params[:id])
+        # クイズの詳細と関連するchoicesとexplanationをJSON形式で返す
         render json: @question, include: [:choices, :explanation], status: :ok
     end
       
+    # todo bebug削除
     def update
         Rails.logger.debug("Original question: #{@question.inspect}")
         Rails.logger.debug("Submitted params: #{question_params.inspect}")
@@ -55,6 +60,11 @@ class Admin::QuizzesController < ApplicationController
     end
 
     def question_params
-        params.require(:quiz).permit(:question_text, choices_attributes: [:choice_text, :is_correct]) #データのオブジェクト名はquiz
+        #データのオブジェクト名はquiz
+        params.require(:quiz).permit(
+            :question_text, 
+            choices_attributes: [:id, :choice_text, :is_correct, :_destroy], 
+            explanation_attributes: [:id, :explanation_text, :_destroy]
+          )
     end 
 end
--- backend/app/models/explanation.rb
+++ backend/app/models/explanation.rb
@@ -1,2 +1,3 @@
 class Explanation < ApplicationRecord
+    belongs_to :question
 end
