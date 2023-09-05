2385fc52c6a83807499fb762d35300cff8955d31
SomaTomita <134721775+SomaTomita@users.noreply.github.com>
2023-09-06 00:59:35 +0900

    test admin_quizzes

--- backend/spec/factories/users.rb
+++ backend/spec/factories/users.rb
@@ -5,5 +5,9 @@ FactoryBot.define do
       email { Faker::Internet.email }
       password { 'password' }
       password_confirmation { 'password' }
+
+      trait :admin do
+        admin { true }
+      end
     end
   end
